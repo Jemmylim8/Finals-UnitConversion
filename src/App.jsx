25d549c6ff7fedbd3b7ec69f1c826a545986a774
Jemmylim8 <jemlimthesec@gmail.com>
2025-05-19 18:54:28 +0800

    added Values need Operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
+import { use } from "react";
 import { useState } from "react";
 
 export default function App() {
   const [selectedValue, setSelectedValue] = useState("");
-  const [input ,setInput] = useState("");
-  const [showHeights,setShowHeights] =useState(true);
-  const [showWeights,setShowWeights] =useState(true);
-  const [choice ,setChoice] = useState("");
+  const [input, setInput] = useState();
+  const [showHeights, setShowHeights] = useState(true);
+  const [showWeights, setShowWeights] = useState(true);
+  const [choice, setChoice] = useState("");
+
+  //HEIGHT
+  const [feet, setFeet] = useState();
+  const [cm,setCm] = useState();
+  const [meter,setMeter] = useState();
+  const [inches,setInches] = useState();
+  //WEIGHT
+  const [pounds,setPounds]=useState();
+  const [kilos,setKilos]=useState();
+  const [grams,setGrams]=useState();
+  const [ounce,setOunce]=useState();
   function handleTypeChange(event) {
     setSelectedValue(event.target.value);
-    if(selectedValue=="Height"){
-        setShowWeights(false);
-        setShowHeights(true);
+    if (selectedValue == "Height") {
+      setShowWeights(false);
+      setShowHeights(true);
     }
-    if(selectedValue=="Weight"){
-        setShowWeights(true);
-        setShowHeights(false);
+    if (selectedValue == "Weight") {
+      setShowWeights(true);
+      setShowHeights(false);
     }
   }
+  const updateValues= ()=>{
+  if(choice==="feet")
+    {
+      setMeter=input*0.3048;
+      setCm=input*30.48;
+      setInches=input*12;
+    }
+  } 
   function handleWeightChange(event) {
     setChoice(event.target.value);
+    
   }
+
   function handleHeightChange(event) {
     setChoice(event.target.value);
   }
-  const showChoices = ()=>{
-    
-  }
+
+  
 
   return (
     <>
@@ -34,17 +55,94 @@ export default function App() {
         <option value="Height">Height</option>
         <option value="Weight">Weight</option>
       </select>
-     <select id="dropdownHeight" onChange={handleWeightChange} hidden={showWeights}>
+
+      <select id="dropdownWeight" onChange={handleWeightChange} hidden={showWeights}>
         <option value="Pounds">Lbs</option>
         <option value="Kilos">Kilos</option>
-     </select>
-     <select id="dropdownHeight" onChange={handleHeightChange} hidden={showHeights}>
-        <option value="feet">in</option>
+        <option value="Grams">Grams</option>
+        <option value="Ounces">Ounces</option>
+      </select>
+
+      <select id="dropdownHeight" onChange={handleHeightChange} hidden={showHeights}>
+        <option value="feet">Feet</option>
         <option value="cm">Cm</option>
-     </select>
+        <option value="meters">Meters</option>
+        <option value="inches">Inches</option>
+      </select>
+
       <button onClick={() => alert(`You selected: ${selectedValue}`)}>Submit</button>
       <p>You selected: {selectedValue} in {choice}</p>
-      <input placeholder="Input Number to Convert"></input>
+      <input placeholder="Input Number to Convert" onChange={(e) =>{ setInput(e.target.value);updateValues;}}></input>
+      <p>{input} {choice}</p>
+
+      <div hidden ={showHeights}>
+        <p>{input} {choice} = feet </p>
+        <p>{input} {choice} = {cm}cm </p>
+        <p>{input} {choice} = {meter}meters</p>
+        <p>{input} {choice} = {inches}inches</p>
+      </div>
+      <div hidden ={showWeights}>
+        <p>{input} {choice} = </p>
+        <p>{input} {choice} = </p>
+        <p>{input} {choice} = </p>
+        <p>{input} {choice} = </p>
+      </div>
     </>
   );
 }
+
+
+
+// import { useState } from "react";
+
+// export default function App() {
+//   const [selectedValue, setSelectedValue] = useState("");
+//   const [input ,setInput] = useState("");
+//   const [showHeights,setShowHeights] =useState(true);
+//   const [showWeights,setShowWeights] =useState(true);
+//   const [choice ,setChoice] = useState("");
+  
+//   function handleTypeChange(event) {
+//     setSelectedValue(event.target.value);
+//     if(selectedValue=="Height"){
+//         setShowWeights(false);
+//         setShowHeights(true);
+//     }
+//     if(selectedValue=="Weight"){
+//         setShowWeights(true);
+//         setShowHeights(false);
+//     }
+//   }
+//   function handleWeightChange(event) {
+//     setChoice(event.target.value);
+//   }
+//   function handleHeightChange(event) {
+//     setChoice(event.target.value);
+//   }
+//   const showChoices = ()=>{
+    
+//   }
+
+//   return (
+//     <>
+//       <h1>UNIT CONVERSION</h1>
+//       <select id="dropdown" onChange={handleTypeChange}>
+//         <option value="Height">Height</option>
+//         <option value="Weight">Weight</option>
+//       </select>
+//      <select id="dropdownHeight" onChange={handleWeightChange} hidden={showWeights}>
+//         <option value="Pounds">Lbs</option>
+//         <option value="Kilos">Kilos</option>
+//      </select>
+//      <select id="dropdownHeight" onChange={handleHeightChange} hidden={showHeights}>
+//         <option value="feet">in</option>
+//         <option value="cm">Cm</option>
+//      </select>
+//       <button onClick={() => alert(`You selected: ${selectedValue}`)}>Submit</button>
+//       <p>You selected: {selectedValue} in {choice}</p>
+//       <input placeholder="Input Number to Convert" 
+//       onChange={(e)=> {setInput(e.target.value);}}></input>
+//       <p>{input} {choice}</p>
+//     </>
+//   );
+// }
